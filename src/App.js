e4a66606465f755e1eda9d6b181489655b217f6a
jorgenunez8526 <jorgenunez8526@gmail.com>
2024-10-27 10:35:41 -0400

    modified Navbar and deleted components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import logo from './logo.svg';
-import React from 'react';
-import Navbar from './component/Navbar';
-import About from './component/About';
-import Projects from './component/Projects';
-import Contact from './component/Contact';
 import './App.css';
 
 const App = () => {
   return (
-      <div>
-          <Navbar />
-          <About />
-          <Projects />
-          <Contact />
+      <div className="App">
+
       </div>
   );
 };
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,25 +1,38 @@
-// Navbar.js
-import React from 'react';
-import styled from 'styled-components';
-
-const Nav = styled.nav`
-    background-color: #f0f0f0;
-    padding: 20px;
-`;
-
-const NavLink = styled.a`
-    margin-right: 20px;
-    text-decoration: none;
-    color: #333;
-`;
-
-const Navbar = () => {
+import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+export const Navbar = () => {
     return (
-        <Nav>
-            <NavLink href="#about">About</NavLink>
-            <NavLink href="#projects">Projects</NavLink>
-            <NavLink href="#contact">Contact</NavLink>
-        </Nav>
+    <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+            <Navbar.Brand href="#home">
+                <img src={''} alt="logo"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+                <span className="navbar-toggler-icon"></span>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav>
+                <span className="navbar-text">
+                    <div className="social-icon">
+                        <div className="social-icon">
+                            <a href="https://github.com/Jorg3Nunez">
+                                <img src={''} alt="github"/>
+                            </a>
+                        </div>
+                        <div className="social-icon">
+                            <a href="https://www.linkedin.com/in/jorgenunez85">
+                                <img src={''} alt="linkedin"/>
+                            </a>
+                        </div>
+                    </div>
+                </span>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
     );
 };
 
